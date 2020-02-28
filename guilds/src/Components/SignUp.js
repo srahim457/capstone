a1a8bad80f8ca3879a869f815b75051ca03c187a
Louis-Ramirez <Louis.Ramirez60@myhunter.cuny.edu>
2020-02-27 23:50:52 -0500

    add

--- guilds/src/Components/SignUp.js
+++ guilds/src/Components/SignUp.js
@@ -1,2 +1,14 @@
 import React from './node_modules/react';
-import React from 'react';
+import React, { Component } from 'react';
+
+class SignUp extends Component() {
+  render() {
+    return (
+      <div>
+        <p>This is the Sign up page</p>
+      </div>
+    );
+  }
+}
+
+export default SignUp;
