ce5b9c68ec583ffde556af3b9aae5fa0d6fb7c08
Louis-Ramirez <Louis.Ramirez60@myhunter.cuny.edu>
2020-02-27 23:08:38 -0500

    push

--- guilds/src/Components/SignUp.js
+++ guilds/src/Components/SignUp.js
@@ -1,1 +1,2 @@
 import React from './node_modules/react';
+import React from 'react';
---
+++ guilds/src/components/Navigation.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+const Navigation = () => {
+  return <div></div>;
+};
+export default Navigation;
