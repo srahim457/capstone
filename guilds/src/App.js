945ab763623ca31f4bf17aa5cfceb05872da5463
Louis-Ramirez <Louis.Ramirez60@myhunter.cuny.edu>
2020-03-08 00:42:08 -0500

    new format

--- guilds/src/App.js
+++ guilds/src/App.js
@@ -8,80 +8,26 @@ import {
   Switch
 } from 'react-router-dom';
 
-import Login from './Components/Login';
-import Signup from './Components/SignUp';
+import Home from './Components/Home';
+import Navigation from './Components/Navigation';
+import Profile from './Components/Profile';
+import MarketPlace from './Components/MarketPlace';
 import './App.css';
 import guildlogo from './images/guildslogo.png';
-
-// function App() {
-//   return (
-//     <BrowserRouter>
-//       <div className='App'>
-//         <Navigation>
-//           <Switch>
-//             <Route path='/' component={Login} exact />
-//           </Switch>
-//         </Navigation>
-//       </div>
-//     </BrowserRouter>
-//   );
-// }
-
-// export default App;
 
 class App extends Component {
   render() {
     return (
-      <Router basename='/react-auth-ui/'>
-        <div className='App'>
-          <div className='App__Aside'>
-            <img src={guildlogo} alt=''></img>
-            <h4 className='caption'>
-              Join to connect and trade within your campus
-            </h4>
-          </div>
-          <div className='App__Form'>
-            <div className='PageSwitcher'>
-              <NavLink
-                to='/sign-in'
-                activeClassName='PageSwitcher__Item--Active'
-                className='PageSwitcher__Item'
-              >
-                Sign In
-              </NavLink>
-              <NavLink
-                exact
-                to='/'
-                activeClassName='PageSwitcher__Item--Active'
-                className='PageSwitcher__Item'
-              >
-                Sign Up
-              </NavLink>
-            </div>
-
-            <div className='FormTitle'>
-              <NavLink
-                to='/sign-in'
-                activeClassName='FormTitle__Link--Active'
-                className='FormTitle__Link'
-              >
-                Sign In
-              </NavLink>{' '}
-              or{' '}
-              <NavLink
-                exact
-                to='/'
-                activeClassName='FormTitle__Link--Active'
-                className='FormTitle__Link'
-              >
-                Sign Up
-              </NavLink>
-            </div>
-
-            <Route exact path='/' component={Signup}></Route>
-            <Route path='/sign-in' component={Login}></Route>
-          </div>
-        </div>
+      <Router>
+        <Switch>
+          <Route exact path='/' component={Home}></Route>
+          <Route path='/profile' component={Profile}>
+            <Profile></Profile>
+          </Route>
+          <Route path='/market-place' component={MarketPlace}>
+            <MarketPlace></MarketPlace>
+          </Route>
+        </Switch>
       </Router>
     );
   }
---
+++ guilds/src/Components/Profile.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import Navigation from './Navigation';
+import { Link } from 'react-router-dom';
+
+class Profile extends Component {
+  render() {
+    return (
+      <div>
+        <h1>This is the profile page </h1>
+        <p>Check it out</p>
+      </div>
+    );
+  }
+}
+
+export default Profile;
