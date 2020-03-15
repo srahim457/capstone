53c96823ec6adb91e8c062c9c1044fa6039fedc1
Louis-Ramirez <Louis.Ramirez60@myhunter.cuny.edu>
2020-03-14 22:02:33 -0400

    push template

--- guilds/src/Components/MarketPlace.js
+++ guilds/src/Components/MarketPlace.js
@@ -1,13 +1,48 @@
 import React, { Component } from 'react';
+import Pagination from './Pagination';
+import '../MarketPlace.css';
 
 class MarketPlace extends Component {
+  constructor() {
+    super();
+
+    // an example array of 150 items to be paged
+    var exampleItems = [...Array(150).keys()].map(i => ({
+      id: i + 1,
+      name: 'Item ' + (i + 1)
+    }));
+
+    this.state = {
+      exampleItems: exampleItems,
+      pageOfItems: []
+    };
+
+    // bind function in constructor instead of render (https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/jsx-no-bind.md)
+    this.onChangePage = this.onChangePage.bind(this);
+  }
+
+  onChangePage(pageOfItems) {
+    // update state with new page of items
+    this.setState({ pageOfItems: pageOfItems });
+  }
+
   render() {
     return (
-      <div>
-        <h1>This is the MarketPlace</h1>
+      <div className='containerParent'>
+        <div className='container'>
+          <h1 className='title'>Market Place</h1>
+          {this.state.pageOfItems.map(item => (
+            <div className='items' key={item.id}>
+              {item.name}
+            </div>
+          ))}
+          <Pagination
+            items={this.state.exampleItems}
+            onChangePage={this.onChangePage}
+          />
+        </div>
       </div>
     );
   }
 }
-
 export default MarketPlace;
---
+++ guilds/src/components/Items.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+class Items extends Component {
+  render() {
+    return <h1>This is an Item</h1>;
+  }
+}
+
+export default Items;
