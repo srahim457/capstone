abe07e32d9f9b6fceafdb6728dd7b5ee169afce5
Louis-Ramirez <Louis.Ramirez60@myhunter.cuny.edu>
2020-03-22 15:35:55 -0400

    prototype

--- guilds/src/Components/MarketPlace.js
+++ guilds/src/Components/MarketPlace.js
@@ -15,11 +15,13 @@ class MarketPlace extends Component {
 
     this.state = {
       exampleItems: exampleItems,
-      pageOfItems: []
+      pageOfItems: [],
+      click: false //added to see if it respond on click
     };
 
     // bind function in constructor instead of render (https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/jsx-no-bind.md)
     this.onChangePage = this.onChangePage.bind(this);
+    this.onClickHandler = this.onClickHandler.bind(this);
   }
 
   onChangePage(pageOfItems) {
@@ -27,23 +29,35 @@ class MarketPlace extends Component {
     this.setState({ pageOfItems: pageOfItems });
   }
 
+  onClickHandler(e) {
+    e.preventDefault();
+    this.setState({ click: true });
+  }
+
   render() {
+    if (this.state.click == true) {
+      return <CreateListing />;
+    }
     return (
       <div className='containerParent'>
         <div className='container'>
           <h1 className='title'>Market Place</h1>
           <div className='button-wrapper'>
-            <button className='listing-button'>Create a Listing</button>
+            <button className='listing-button' onClick={this.onClickHandler}>
+              Create a Listing
+            </button>
           </div>
           {this.state.pageOfItems.map(item => (
             <div className='items' key={item.id}>
               {item.name}
             </div>
           ))}
-          <Pagination
-            items={this.state.exampleItems}
-            onChangePage={this.onChangePage}
-          />
+          <div className='paginate'>
+            <Pagination
+              items={this.state.exampleItems}
+              onChangePage={this.onChangePage}
+            />
+          </div>
         </div>
       </div>
     );
--- guilds/src/components/CreateListing.js
+++ guilds/src/components/CreateListing.js
@@ -2,8 +2,50 @@ import React, { Component } from 'react';
 import './styles/CreateListing.css';
 
 class CreateListing extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      name: '',
+      price: '',
+      description: ''
+    };
+  }
+
   render() {
-    return <h1>Items Component</h1>;
+    return (
+      // <div>{this.props.children}</div>
+
+      <div className='container-parent'>
+        <div className='container'>
+          <h1 className='title'>Create Listing</h1>
+          <form onSubmit={this.handleSubmit} className='form-fields'>
+            <div>
+              <div className='button-wrapper'>
+                <button className='close-button'>X</button>
+              </div>
+              <label>Item Name </label>
+
+              <br />
+              <input type='text' className='form-input'></input>
+            </div>
+            <div>
+              {/* make into a description box */}
+              <label>Item Description</label>
+              <br />
+              <input type='text' className='form-input'></input>
+            </div>
+            <div>
+              <label>
+                Input the time and date that you want the item returned
+              </label>
+              <br />
+              <input type='datetime-local'></input>
+            </div>
+          </form>
+        </div>
+      </div>
+    );
   }
 }
 
