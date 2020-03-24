77a3c430455024a86a01a12575f3b8d107542aee
srahim457 <srahim457@gmail.com>
2020-03-24 12:33:24 -0400

    added new .js files to display items

---
+++ guilds/src/components/Profile_Listed.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import Navigation from './Navigation';
+import {
+  BrowserRouter,
+  HashRouter as Router,
+  Route,
+  Link,
+  NavLink,
+  Switch
+} from 'react-router-dom';
+
+import './styles/profile.css';
+
+class Profile_Listed extends Component {
+  render() {
+    return (
+      <div>
+        <h1> My Listed Items </h1>
+      </div>
+    );
+  }
+}
+
+export default Profile_Listed;
