f1616113d9695278d97e13629e3f613437f4cd2c
srahim457 <srahim457@gmail.com>
2020-03-24 12:36:48 -0400

    updated profile page

--- guilds/src/components/Profile.js
+++ guilds/src/components/Profile.js
@@ -1,9 +1,24 @@
 import React, { Component } from 'react';
 import Navigation from './Navigation';
-import { Link } from 'react-router-dom';
+import {
+  BrowserRouter,
+  HashRouter as Router,
+  Route,
+  Link,
+  NavLink,
+  Switch
+} from 'react-router-dom';
+
+import './styles/profile.css';
+import Profile_Borrowed from './Profile_Borrowed';
+import Profile_Listed from './Profile_Listed';
 
 function Nombre(props) {
-  return <h1>This is the {props.name} page</h1>;
+  return <h1>{props.name}</h1>;
+}
+
+function Rango(props) {
+  return <h1>{props.status}</h1>;
 }
 
 // const element = <Nombre name='Profile' />;
@@ -11,11 +26,50 @@ function Nombre(props) {
 class Profile extends Component {
   render() {
     return (
-      <div>
-        <Nombre name='Profile' />
+      <BrowserRouter>
+      <div className='Background'>
 
-        <p>Check it out</p>
+        <div className='Header'>
+          <div className='NameField'>
+            <img src='../images/guildslogo.png' alt=''></img>
+          </div>
+          <div className='NameField'>
+            <h1> My Name: </h1>
+          </div>
+          <div className='NameField'>
+            <Nombre name='Rouis Lamirez'/>
+          </div>
+          <div className='NameField'>
+            <h1> Rank: </h1>
+          </div>
+          <div className='NameField'>
+            <Rango status='Most Honorable'/>
+          </div>
+        </div>
+
+        <div className='Listings'>
+          <div className='PageSwitcher'>
+            <NavLink
+              to='/profile-borrowed'
+              activeClassName='PageSwitcher__Item--Active'
+              className='PageSwitcher__Item'
+            >
+              Borrowed Items
+            </NavLink>
+            <NavLink
+              exact
+              to='/profile-listed'
+              activeClassName='PageSwitcher__Item--Active'
+              className='PageSwitcher__Item'
+            >
+              Listed Items
+            </NavLink>
+          </div>
+          <Route path='/profile-listed' component={Profile_Listed}></Route>
+          <Route path='/profile-borrowed' component={Profile_Borrowed}></Route>
+        </div>
       </div>
+      </BrowserRouter>
     );
   }
 }
