a2de30009f13ac9912255b71e8e92a3998f96e8c
rsef <rseferi999@gmail.com>
2020-03-23 23:12:47 -0400

    db info

---
+++ guilds/app/model/db.js
@@ -0,0 +1,18 @@
+const { Pool } = require('pg')
+
+const pool = new Pool({
+    user: process.env.USER,
+    host: process.env.HOST,
+    database: process.env.DATABASE,
+    password: process.env.PASSWORD,
+    post: 5432,
+    connectionString: process.env.DATABASE_URL,
+    ssl: true,
+    sslmode: require
+  })
+
+pool.connect(function(err) {
+    if (err) throw err;
+});
+
+module.exports = pool;
