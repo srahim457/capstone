c5eb7c1f3ec0fef26b20a67dfb455076e09a4ddc
rsef <rseferi999@gmail.com>
2020-03-23 23:43:57 -0400

    static files

--- guilds/app/server.js
+++ guilds/app/server.js
@@ -19,8 +19,14 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 // Heroku
-app.use(express.static(path.join(__dirname, 'guilds/build')));
+if (process.env.NODE_ENV === 'production') {
+	app.use(express.static('guilds/build'));
+}
 var routes = require('./routes/appRoutes.js'); //importing route
 routes(app); //register the route
+
+app.get('/', (request, response) => {
+	response.sendFile(path.join(__dirname, 'guilds/build', 'index.html'));
+});
 
 console.log('API server started on: ' + port);
