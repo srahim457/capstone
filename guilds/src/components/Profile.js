8ddfe9367a0846fb5d440bdf5572932cafb5b4c5
Louis-Ramirez <Louis.Ramirez60@myhunter.cuny.edu>
2020-04-06 17:45:30 -0400

    test commit

--- guilds/src/components/Profile.js
+++ guilds/src/components/Profile.js
@@ -5,12 +5,13 @@ import {
   Route,
   Link,
   NavLink,
-  Switch
+  Switch,
 } from 'react-router-dom';
 
 import './styles/profile.css';
 import Profile_Borrowed from './Profile_Borrowed';
 import Profile_Listed from './Profile_Listed';
+import EditProfile from './EditProfile';
 import LaserLouis from '../images/LaserLouis.jpg';
 
 function Nombre(props) {
@@ -27,9 +28,9 @@ class Profile extends Component {
   constructor() {
     super();
     this.state = {
-      click: false //added to see if it respond on click
+      click: false, //added to see if it respond on click
     };
-    
+
     this.onClickHandler = this.onClickHandler.bind(this);
   }
   onClickHandler(e) {
@@ -38,80 +39,88 @@ class Profile extends Component {
   }
 
   render() {
-    {/*if the edit profile button is pressed it will redirect*/}
+    {
+      /*if the edit profile button is pressed it will redirect*/
+    }
     if (this.state.click == true) {
       return <EditProfile />;
     }
     return (
       <BrowserRouter>
-
-      <div className= 'Background'>
-        {/*The user information*/}
-        <div className='Header'>
-          <div className='ProfilePic'> {/*adding the profile pic*/}
-            <img src={LaserLouis} alt=''></img>
-          </div>
-          <div className='button-container'> {/*the button that change the page to edit profile information*/}
-            <button class="edit-button" onClick={this.onClickHandler}>
-            Edit
-            </button>
-          </div>
-          <div className='UserInfoContainer'>
-            <div className='HeaderField'> {/*the username*/}
-              <h1>My Name: </h1>
-              <div className='UserField'>
-                <h1>Rouis Lamirez</h1>
+        <div className='Background'>
+          {/*The user information*/}
+          <div className='Header'>
+            <div className='ProfilePic'>
+              {' '}
+              {/*adding the profile pic*/}
+              <img src={LaserLouis} alt=''></img>
+            </div>
+            <div className='button-container'>
+              {' '}
+              {/*the button that change the page to edit profile information*/}
+              <button class='edit-button' onClick={this.onClickHandler}>
+                Edit
+              </button>
+            </div>
+            <div className='UserInfoContainer'>
+              <div className='HeaderField'>
+                {' '}
+                {/*the username*/}
+                <h1>My Name: </h1>
+                <div className='UserField'>
+                  <h1>Rouis Lamirez</h1>
+                </div>
+              </div>
+              <div className='HeaderField'>
+                {' '}
+                {/*the guild ranking*/}
+                <h1>My Rank: </h1>
+                <div className='UserField'>
+                  <h1>Most Honorable</h1>
+                </div>
               </div>
             </div>
-            <div className='HeaderField'> {/*the guild ranking*/}
-              <h1>My Rank: </h1>
-              <div className='UserField'>
-                <h1>Most Honorable</h1>
+            <div className='SubfieldInfoContainer'>
+              <div className='HeaderSubfield'>
+                <h2>Email: </h2>
+              </div>
+              <div className='UserSubfield'>
+                <h2> rouis.lamirez@myuniversity.edu </h2>
+              </div>
+              <div className='HeaderSubfield'>
+                <h2>Phone: </h2>
+              </div>
+              <div className='UserSubfield'>
+                <h2> ###-###-#### </h2>
               </div>
             </div>
           </div>
-          <div className='SubfieldInfoContainer'>
-            <div className='HeaderSubfield'>
-              <h2>Email: </h2>
+
+          {/*The user's listings*/}
+          <div className='Listings'>
+            {/*the button that switched tabs between items listed and borrowed*/}
+            <div className='PageSwitcher_profile'>
+              <NavLink
+                to='/profile'
+                activeClassName='PageSwitcher__Item--Active'
+                className='PageSwitcher__Item_profile'
+              >
+                Borrowed Items
+              </NavLink>
+              <NavLink
+                exact
+                to='/profile-listed'
+                activeClassName='PageSwitcher__Item--Active'
+                className='PageSwitcher__Item_profile'
+              >
+                Listed Items
+              </NavLink>
             </div>
-            <div className='UserSubfield'>
-              <h2> rouis.lamirez@myuniversity.edu </h2>
-            </div>
-            <div className='HeaderSubfield'>
-              <h2>Phone: </h2>
-            </div>
-            <div className='UserSubfield'>
-              <h2> ###-###-#### </h2>
-            </div>
+            {/*these components can be found in Profile_Borrowed.js and Profile_Listed.js*/}
+            <Route path='/profile-listed' component={Profile_Listed}></Route>
+            <Route path='/profile' component={Profile_Borrowed}></Route>
           </div>
         </div>
-
-        {/*The user's listings*/}
-        <div className='Listings'>
-        {/*the button that switched tabs between items listed and borrowed*/}
-        <div className='PageSwitcher_profile'>
-          <NavLink
-            to='/profile'
-            activeClassName='PageSwitcher__Item--Active'
-            className='PageSwitcher__Item_profile'
-          >
-            Borrowed Items
-          </NavLink>
-          <NavLink
-            exact
-            to='/profile-listed'
-            activeClassName='PageSwitcher__Item--Active'
-            className='PageSwitcher__Item_profile'
-          >
-            Listed Items
-          </NavLink>
-        </div>
-        {/*these components can be found in Profile_Borrowed.js and Profile_Listed.js*/}
-        <Route path='/profile-listed' component={Profile_Listed}></Route>
-        <Route path='/profile' component={Profile_Borrowed}></Route>
-        </div>
-      </div>
-
       </BrowserRouter>
     );
   }
