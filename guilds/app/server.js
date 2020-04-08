5e865686f59e6708b9c4e2f2ed72c1ca9235b040
Louis-Ramirez <Louis.Ramirez60@myhunter.cuny.edu>
2020-04-07 23:26:13 -0400

    changed vars to const

--- guilds/app/server.js
+++ guilds/app/server.js
@@ -1,32 +1,35 @@
-var express = require('express');
+const express = require('express');
 
-var bodyParser = require('body-parser');
-var session = require('express-session')
-var cors = require('cors');
-var path = require('path')
+const bodyParser = require('body-parser');
+const session = require('express-session');
+const cors = require('cors');
+const path = require('path');
 
 port = process.env.PORT || 4000;
 
 // connection configurations
-var db = require('./model/db.js') 
+const db = require('./model/db.js');
 
-var app = express();
+const app = express();
 app.listen(port);
 app.use(cors());
+
 //parse json/application
 app.use(bodyParser.json());
+
 //parse urlencoded
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static("public"));
+app.use(express.static('public'));
+
 // Heroku
 if (process.env.NODE_ENV === 'production') {
-	app.use(express.static('../build'));
+  app.use(express.static('../build'));
 }
-var routes = require('./routes/appRoutes.js'); //importing route
+const routes = require('./routes/appRoutes.js'); //importing route
 routes(app); //register the route
 
 app.get('/', (request, response) => {
-	response.sendFile(path.join(__dirname, '../build', 'index.html'));
+  response.sendFile(path.join(__dirname, '../build', 'index.html'));
 });
 
 console.log('API server started on: ' + port);
