43343531c88e2d01b0ccc9549fec50fe6a6af29c
srahim457 <srahim457@gmail.com>
2020-04-14 11:41:32 -0400

    populates all the profile listing with info

--- guilds/src/components/Profile_Borrowed.js
+++ guilds/src/components/Profile_Borrowed.js
@@ -10,7 +10,14 @@ class Profile_Borrowed extends Component {
     var items = tmp.map(function (i) {
       return (
         <div className='item'>
-
+          <div className='itemImageWrapper'>
+            <h1> img {i} </h1>
+          </div>
+          <div className='itemInfoWrapper'>
+            <h1 className='itemInfoField'> Name: Item {i}</h1>
+            <h1 className='itemInfoField'> Cost: ${i}</h1>
+            <h1 className='itemInfoField'> Borrow Date: #/#/#  Return Date: #/#/# </h1>
+          </div>
         </div>
       );
     });
