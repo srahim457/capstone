6da8039094c8dd56dc4a68864ed1b91311a5062e
srahim457 <srahim457@gmail.com>
2020-04-14 11:38:51 -0400

    Updated AllGuild.js

--- guilds/src/components/AllGuilds.js
+++ guilds/src/components/AllGuilds.js
@@ -1,12 +1,65 @@
 import React, { Component } from 'react';
-//import './styles/Messages.css';
+import Navigation from './Navigation';
+import {
+  BrowserRouter,
+  HashRouter as Router,
+  Route,
+  Link,
+  NavLink,
+  Switch
+} from 'react-router-dom';
+
+import './styles/AllGuilds.css';
+
+
 
 class AllGuilds extends Component {
-  render() {
-    return (
-      <div className='container-parent'>
-        <div className='container'>
-          <h1 className='title'>All Guilds</h1>
+
+  render(){
+    var tmp = [];
+    for (var i = 0; i < 10; i++) {
+      tmp.push(i);
+    }
+
+    var guilds = tmp.map(function (i) {
+      return (
+        <div className='GuildContainer'>
+          <div className='GuildImageWrapper'>
+            <h1> Image {i} </h1>
+          </div>
+          <div className='GuildNameWrapper'>
+            <h1> guild {i} </h1>
+          </div>
+        </div>
+      );
+    });
+
+    return(
+      <div className='Background'> {/*the background color*/}
+        <h1 className='title'>All Guilds</h1>
+        
+
+        <div className='GuildBoard'>
+          <div className='addGuildsButtonWrapper'>
+            <button class="add-button">
+            Add New Guild
+            </button>
+          </div>
+
+          <div className='searchWrapper'>
+            <input
+              type='text'
+              className='search-input'
+              placeholder='search for guilds'
+              maxLength='200'
+            ></input>
+            <button class="search-button">
+              Search
+            </button>
+          </div>
+
+          {guilds}
+
         </div>
       </div>
     );
