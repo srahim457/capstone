8eafd56d406a4a487d71f95734a0bab951db20a9
Louis-Ramirez <Louis.Ramirez60@myhunter.cuny.edu>
2020-04-18 11:34:18 -0400

    push changes

--- guilds/app/lib/db.js
+++ guilds/app/lib/db.js
@@ -1,20 +1,24 @@
 const { Pool } = require('pg');
+require('dotenv').config({ path: '../../.env' });
 
 const pool = new Pool({
-    user: process.env.USER,
-    host: process.env.HOST,
-    database: process.env.DATABASE,
-    password: process.env.PASSWORD,
-    post: 5432,
-    connectionString: process.env.DATABASE_URL,
-    ssl: true,
-    sslmode: require
-  })
+  user: process.env.USER,
+  host: process.env.HOST,
+  database: process.env.DATABASE,
+  password: process.env.PASSWORD,
+  port: 5432,
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+  sslmode: require,
+  max: 20,
+});
 
-pool.connect(function(err) {
-    if (err) throw err;
+pool.connect(function (err) {
+  if (err) throw err;
 });
 
 module.exports = {
-  pool
-}
+  pool,
+};
--- guilds/app/server.js
+++ guilds/app/server.js
@@ -10,14 +10,16 @@ const PORT = process.env.PORT || 4000;
 
 var flash = require('connect-flash');
 
-var session = require('express-session')
-var passport = require("passport");
+var session = require('express-session');
+var passport = require('passport');
 var request = require('request');
 
 var app = express();
 
 app.use(cors());
-app.use(session({secret: 'capstone',resave: 'false',saveUninitialized: 'false'}))
+app.use(
+  session({ secret: 'capstone', resave: 'false', saveUninitialized: 'false' })
+);
 app.use(passport.initialize());
 app.use(passport.session());
 
@@ -25,10 +27,10 @@ app.use(passport.session());
 app.use(bodyParser.json());
 //parse urlencoded
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static("public"));
+app.use(express.static('public'));
 // Heroku
 if (process.env.NODE_ENV === 'production') {
-	app.use(express.static('../build'));
+  app.use(express.static('../build'));
 }
 //var routes = require('./routes/appRoutes.js'); //importing route
 //routes(app); //register the route
@@ -41,10 +43,9 @@ app.use(flash());
 app.set('view engine', 'pug');
 app.set('view options', { layout: false });
 
-
 require('./lib/routes.js')(app);
-require('./lib/forgotPassword.js')(app);
-require('./lib/resetPassword.js')(app);
+// require('./lib/forgotPassword.js')(app);
+// require('./lib/resetPassword.js')(app);
 
 app.listen(PORT);
 console.log('Node listening on port %s', PORT);
