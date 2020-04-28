76fcd51ce74cdb8e259df673d4e591a568437c89
rsef <rseferi999@gmail.com>
2020-04-28 13:32:22 -0400

    updated listing

--- guilds/app/lib/models/Listing.js
+++ guilds/app/lib/models/Listing.js
@@ -19,8 +19,8 @@ var Listing = function (listing) {
 
 //Create a new listing entry
 //Returns listing id
-Listing.createListing = function (listing, result) {
-  console.log('creating new listing w/', listing),
+Listing.createListing = function (req,res) {
+  console.log('creating new listing \n'),
     sql.query(
       'INSERT INTO guilds.listings(item_id,time_posted,total_price,rent_amount,insurance_amount,lender_id) VALUES($1,$2,$3,$4,$5,$6) RETURNING *',
       [
@@ -31,114 +31,106 @@ Listing.createListing = function (listing, result) {
         listing.insurance_amount,
         listing.lender_id,
       ],
-      function (err, res) {
+      function (err, resp) {
         if (err) {
-          console.log('error: ', err);
-          result(err, null);
+          console.log("error creating listing ", err);
+          res.status(400);
         } else {
-          console.log(res.rows[0]);
-          result(null, res.rows[0].id);
+          res.status(200).send(resp.rows[0]);
         }
       }
     );
 };
 //Return listing that matches listing id
 //Only takes in the listing id as a parameter
-Listing.getListingByID = function (listing_id, result) {
+Listing.getListingByID = function (req,res) {
   sql.query(
-    'Select * from guilds.listings where id = ANY ($1)',
-    [listing_id],
-    function (err, res) {
+    'Select * from guilds.listings where id =($1)',
+    [req.listing_id],
+    function (err, resp) {
       if (err) {
-        console.log('error: ', err);
-        result(err, null);
+        console.log("error getting listing by id ", err);
+        res.status(400);
       } else {
-        console.log('found the listing');
-        result(null, res);
+        res.status(200).send(resp.rows[0]);
       }
     }
   );
 };
 //Return all listings by a certain borrower id
-Listing.getAllBorrowerListings = function (user_id, result) {
+Listing.getAllBorrowerListings = function (req,res) {
   sql.query(
-    'Select * from guilds.listings where borrower_id = ANY ($1)',
-    [user_id],
-    function (err, res) {
+    'Select * from guilds.listings where borrower_id = ($1)',
+    [req.user_id],
+    function (err, resp) {
       if (err) {
-        console.log('error: ', err);
-        result(err, null);
+        console.log("error getting all borrower listings ", err);
+        res.status(400);
       } else {
-        console.log('found the listings from user');
-        result(null, res);
+        res.status(200).send(resp.rows);
       }
     }
   );
 };
 //Return all listings by a certain lender id
-Listing.getAllLenderListings = function (user_id, result) {
+Listing.getAllLenderListings = function (req,res) {
   sql.query(
-    'Select * from guilds.listings where lender_id = ANY ($1)',
-    [user_id],
-    function (err, res) {
+    'Select * from guilds.listings where lender_id = ($1)',
+    [req.user_id],
+    function (err, resp) {
       if (err) {
-        console.log('error: ', err);
-        result(err, null);
+        console.log("error getting all lender listings ", err);
+        res.status(400);
       } else {
-        console.log('found the listings from lender');
-        result(null, res);
+        res.status(200).send(resp.rows);
       }
     }
   );
 };
 //Updates listing to add a borrower
 //Takes in user_id and listing_id
-Listing.addBorrower = function (ids, result) {
+Listing.addBorrower = function (req, res) {
   sql.query(
-    'UPDATE guilds.listings SET borrower_id = ($1) where id = ANY ($2)',
-    [ids.user_id, ids.listing_id],
-    function (err, res) {
+    'UPDATE guilds.listings SET borrower_id = ($1) where id = ($2)',
+    [req.user_id, req.listing_id],
+    function (err, resp) {
       if (err) {
-        console.log('error: ', err);
-        result(err, null);
+        console.log("error adding a borrower ", err);
+        res.status(400);
       } else {
-        //Input
-        //
-        console.log('Updated listing to include borrower');
-        result(null, res.rows[0].id);
+        res.status(200);
       }
     }
   );
 };
 // Updates the listing to be marked completed
 // Takes in the completed status,time,listing_id
-Listing.markCompleted = function (listing, result) {
+Listing.markCompleted = function (req,res) {
   sql.query(
-    'UPDATE guilds.listings SET completed = ($1), time_sold_expired = ($2) WHERE id = ANY ($3)',
-    [listing.completed, listing.time_sold_expired, listing.listing_id],
-    function (err, res) {
+    'UPDATE guilds.listings SET completed = ($1), time_sold_expired = ($2) WHERE id = ($3)',
+    [req.completed, req.time_sold_expired, req.listing_id],
+    function (err, resp) {
       if (err) {
-        console.log('error: ', err);
-        result(err, null);
+        console.log("error marking complete ", err);
+        res.status(400);
       } else {
-        console.log('Updated listing to include borrower');
-        result(null, res.rows[0].id);
+        res.status(200);
       }
     }
   );
 };
 //Deletes a listing
 //Only takes in listing id
-Listing.deleteItem = function (listing_id) {
+Listing.deleteItem = function (req,res) {
   sql.query(
     "UPDATE guilds.listings SET delete = 'T' where id = ($1)",
-    [listing_id],
-    function (err, res) {
+    [req.listing_id],
+    function (err, resp) {
       if (err) {
-        console.log('error deleting item: ', err);
-        result(err, null);
+        console.log("error deleting listed item ", err);
+        res.status(400);
       } else {
-        console.log('Deleted item');
+        res.status(200);
       }
     }
   );
