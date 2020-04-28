b06239abe3659a86293f77869a2e7bdb5b2e51dd
rsef <rseferi999@gmail.com>
2020-04-28 13:23:18 -0400

    updated login

--- guilds/app/lib/models/Login.js
+++ guilds/app/lib/models/Login.js
@@ -11,29 +11,28 @@ var Login = function (login) {
 
 //Create a new login entry
 //Returns the whole login entry
-Login.createLogin = function (email, pass, result) {
+Login.createLogin = function (req, res) {
   //Checking for duplicate emails
-  sql.query('SELECT * from guilds.login where email = $1', [email], function (
+  sql.query('SELECT * from guilds.login where email = $1', [req.email], function (
     err,
-    res
+    resp
   ) {
-    console.log('Checking if email ', email, ' exists');
-    if (res.rows.length > 0) {
+    console.log('Checking if email ', req.email, ' exists');
+    if (resp.rows.length > 0) {
       console.log('Email already exists');
       result(0, null);
     } else {
       console.log('Email is new');
-      console.log(pass, email);
+      console.log(req.pass, req.email);
       sql.query(
         'INSERT INTO guilds.login (EMAIL,PASSWORD) VALUES ($1,$2) ',
-        [email, pass],
-        function (err, res) {
+        [req.email, req.pass],
+        function (err, respon) {
           if (err) {
-            console.log('error: ', err);
-            result(err, null);
+            console.log("error inserting login ", err);
+            res.status(400);
           } else {
-            console.log(res.rows[0]);
-            result(null, res.rows[0]);
+            res.status(200).send(respon.rows[0]);
           }
         }
       );
@@ -42,54 +41,50 @@ Login.createLogin = function (email, pass, result) {
 };
 // Set both reset tokens for a login
 // Returns login id
-Login.forgotPassword = function (reset_info) {
+Login.forgotPassword = function (req,res) {
   var curr_date = new Date();
-  curr_date.setTime(reset_info.resetPasswordExpires);
+  curr_date.setTime(req.resetPasswordExpires);
   sql.query(
     'UPDATE guilds.login SET reset_pw_tkn =($1), reset_pw_expires =($2) WHERE email =($3) RETURNING *',
-    [reset_info.resetPasswordToken, curr_date.toDateString(), reset_info.email],
-    function (err, res) {
+    [req.resetPasswordToken, curr_date.toDateString(), req.email],
+    function (err, resp) {
       if (err) {
-        console.log('error: ', err);
-        result(err, null);
+        console.log("error updating login ", err);
+        res.status(400);
       } else {
-        console.log('completed forgot password');
-        console.log(res.rows[0].id);
+        res.status(200).send(resp.rows[0]);
       }
     }
   );
 };
 //Return login entry
-Login.findByToken = function (login_info, result) {
+Login.findByToken = function (req,res) {
   var curr_date = new Date();
   curr_date.setTime(login_info.resetPasswordExpires);
   sql.query(
-    'Select * from guilds.login where resetPasswordToken = ANY ($1) and resetPasswordExpires > ($2)',
-    [login_info.resetPasswordToken, curr_date.toDateString()],
-    function (err, res) {
+    'Select * from guilds.login where resetPasswordToken = ($1) and resetPasswordExpires > ($2)',
+    [req.resetPasswordToken, curr_date.toDateString()],
+    function (err, resp) {
       if (err) {
-        console.log('error: ', err);
-        result(err, null);
+        console.log("error finding login ", err);
+        res.status(400);
       } else {
-        console.log('completed token find');
-        console.log(res.rows[0]);
-        result(null, res);
+        res.status(200).send(resp.rows[0]);
       }
     }
   );
 };
 //Return login entry
-Login.findByEmail = function (login_info, result) {
+Login.findByEmail = function (req,res) {
   sql.query(
-    'Select * from guilds.login where email = ANY ($1)',
+    'Select * from guilds.login where email = ($1)',
     [login_info],
-    function (err, res) {
+    function (err, resp) {
       if (err) {
-        console.log('error: ', err);
-        result(err, null);
+        console.log("error finding login by email ", err);
+        res.status(400);
       } else {
-        console.log('completed login find');
-        result(null, res);
+        res.status(200).send(resp.rows[0]);
       }
     }
   );
