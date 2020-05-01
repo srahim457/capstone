e696c46ee048f47e13b12aa7aaa4249c57c30f5c
Louis-Ramirez <Louis.Ramirez60@myhunter.cuny.edu>
2020-05-01 00:17:16 -0400

    profile route put req update

--- guilds/app/lib/routes/profile/profile.js
+++ guilds/app/lib/routes/profile/profile.js
@@ -10,14 +10,16 @@ let Login = require('../../models/Login').Login;
 let Listing = require('../../models/Listing').Listing;
 let Item = require('../../models/Item').Item;
 
+let userID;
 // @route GET profile/me
-// @desc  Get current user profile
+// @desc  Get logged in user profile
 // @access private
 router.get('/me', auth, async (req, res) => {
   try {
     const { email, password } = req.body;
     const profile = await User.getUserByEmail([email], res); //finds user using email instead of id
-    console.log(profile[0]);
+    userID = profile[0].id;
+    //console.log(profile[0].id);
     if (profile == 0) {
       return res
         .status(400)
@@ -30,9 +32,63 @@ router.get('/me', auth, async (req, res) => {
   }
 });
 
-// @route /profile
+// @route UPDATE /profile
 // @desc Update users profile
 // @access Private
-router.put('/', auth, async (req, res) => {});
+router.put(
+  '/',
+  [auth, [check('email', 'email is required').not().isEmpty()]],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (errors.isEmpty() == 0) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    const {
+      firstname,
+      lastname,
+      username,
+      email,
+      online,
+      phonenum,
+      profilepic,
+      rating,
+      id,
+    } = req.body;
+
+    // Build profile object
+    const profileFields = {};
+    if (firstname) profileFields.firstname = firstname;
+    if (lastname) profileFields.lastname = lastname;
+    if (username) profileFields.username = username;
+    if (email) profileFields.email = email;
+    if (online) profileFields.online = online;
+    if (phonenum) profileFields.phonenum = phonenum;
+    if (profilepic) profileFields.profilepic = profilepic;
+    if (rating) profileFields.rating = rating;
+
+    //console.log('Reqbody', email);
+
+    try {
+      console.log('USERID', userID);
+      let profile = await User.getUserById([49], res);
+      //console.log('id:', profile[0]);
+      console.log('emaill', email);
+      //let profile = await User.getUserByEmail([email], res);
+
+      // //console.log('profile', profile);
+      let newProfile;
+      if (profile) {
+        //UPDATE
+        console.log('in here!!');
+        newProfile = await User.updateEmail(email, res); //update profile here
+        console.log(newProfile);
+        //return res.json(profile[0].email);
+      }
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server Error');
+    }
+  }
+);
 
 module.exports = router;
