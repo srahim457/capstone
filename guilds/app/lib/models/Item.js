0546ee2458acd71adde524eb7a2353d5dc4ce8af
rsef <rseferi999@gmail.com>
2020-05-04 20:37:36 -0400

    item converted into async

--- guilds/app/lib/models/Item.js
+++ guilds/app/lib/models/Item.js
@@ -12,100 +12,79 @@ var Item = function (item) {
 //Item
 //Creates a new item entry
 //Returns item id
-Item.createItem = function (req,res) {
-  sql.query(
-    'INSERT INTO guilds.item_info (item_name,item_desc,image,is_available) VALUES($1,$2,$3,$4)',
-    [req.item_name, req.item_desc, req.item_image, req.is_available],
-    function (err, resp) {
-      if (err) {
-        console.log('error: ', err);
-        res.status(400);
-      }
-      else{
-          res.status(200).send(resp.rows[0].id);
-      }
-    }
-  );
+Item.createItem = async function (req, res) {
+  try {
+    console.log('creating item', req[0])
+    const item = await sql.query('INSERT INTO guilds.item_info (item_name,item_desc,image,is_available) VALUES($1,$2,$3,$4)', [req[0].item_name, req[0].item_desc, req[0].item_image, req[0].is_available])
+    return item.rows[0].id
+  } catch (error) {
+    console.log(error)
+    res.status(400)
+  }
 };
 //Return item that matches item id
-Item.getItemByID = function (req, res) {
-  sql.query(
-    'Select * from guilds.item_info where item_id = ANY ($1)',
-    [req.item_id],
-    function (err, resp) {
-      if (err) {
-        console.log('error: ', err);
-        res.status(400);
-      }
-      else{
-          res.status(200).send(resp.rows[0]);
-      }
-    }
-  );
+//Takes in an id
+//Returns item entry
+Item.getItemByID = async function (req, res) {
+  try {
+    console.log('Getting item with id ', req)
+    const item = await sql.query('Select * from guilds.item_info where item_id = ANY ($1)', [req])
+    return item.rows
+  } catch (error) {
+    console.log(error)
+    res.status(400)
+  }
 };
 //Return any item matching the name provided
-Item.getItemByName = function (req,res) {
-  sql.query(
-    'Select * from guilds.item_info where item_id LIKE ($1)',
-    [req.item_name],
-    function (err, resp) {
-      if (err) {
-        console.log('error: ', err);
-        res.status(400);
-      }
-      else{
-          res.status(200).send(resp.rows);
-      }
-    }
-  );
+//Takes in an item name
+//Returns all rows matching passed name
+Item.getItemByName = async function (req, res) {
+  try {
+    console.log('Getting every item with the name ', req)
+    const items = await sql.query('Select * from guilds.item_info where item_name LIKE ($1)', [req])
+    return items.rows
+  } catch (error) {
+    console.log(error)
+    res.status(400)
+  }
 };
 //Updates an item's information (name,desc,image)
-//Returns new item ID
-Item.updateItemInformation = function (req,res) {
-  console.log('updating item information', item);
-  sql.query(
-    'UPDATE guilds.item_info SET item_name=($1), item_desc =($2),image = ($3) WHERE item_id = ($4)',
-    [req.item_name, req.item_desc, req.item_image, req.id],function (err, resp) {
-    if (err) {
-      console.log('error updating item information: ', err);
-      res.status(400);
-    }
-    else{
-        res.status(200).send(resp.rows[0].id);
-    }
-  });
+//Returns item ID of updated item
+Item.updateItemInformation = async function (req, res) {
+  try {
+    console.log('updating item information with this new information', req);
+    const item = await sql.query('UPDATE guilds.item_info SET item_name=($1), item_desc =($2),image = ($3) WHERE item_id = ($4)', [req[0].item_name, req[0].item_desc, req[0].item_image, req[0].id])
+    return item.rows[0].id
+  } catch (error) {
+    console.log(error)
+    res.status(400)
+  }
 };
 //Sets the items availability
-Item.updateAvailability = function (req,res) {
-  console.log('updating item Availability', item);
-  sql.query(
-    'UPDATE guilds.item_info SET is_available=($1) WHERE item_id = ($2)',
-    [req.is_available, req.id], function (err, resp) {
-    if (err) {
-      console.log('error updating item availability: ', err);
-      res.status(400);
-    }
-    else{
-        res.status(200);
-    }
-  });
+//Takes item id and is_availiable 
+//Returns avail stat
+Item.updateAvailability = async function (req, res) {
+  try {
+    console.log('updating item Availability with ', req);
+    const item = await sql.query('UPDATE guilds.item_info SET is_available=($1) WHERE item_id = ($2)', [req[0].is_available, req[0].id])
+    return item.rows[0].is_available
+  } catch (error) {
+    console.log(error)
+    res.status(400)
+  }
 };
 //"Deletes" item
 //Takes in only item id
-Item.deleteItem = function (req,res) {
-  sql.query(
-    "UPDATE guilds.item_info SET delete = 'T' where id = ($1)",
-    [req.item_id],
-    function (err, resp) {
-      if (err) {
-        console.log('error deleting item: ', err);
-        res.status(400);
-      }
-      else{
-          res.status(200);
-      }
-    }
-  );
+//Returns item deleted status
+Item.deleteItem = async function (req, res) {
+  try {
+    console.log('deleting an item with id ', req)
+    const item = await sql.query("UPDATE guilds.item_info SET delete = 'T' where id = ($1)", [req])
+    return item.rows[0].delete
+  } catch (error) {
+    console.log(error)
+    res.status(400)
+  }
 };
 
 module.exports = {
