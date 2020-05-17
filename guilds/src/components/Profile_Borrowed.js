1870fbe367d3b08e9c5b39b37a26dbfd024047cb
Louis-Ramirez <Louis.Ramirez60@myhunter.cuny.edu>
2020-05-17 18:49:23 -0400

    fixed profile borrowed images

--- guilds/src/components/Profile_Borrowed.js
+++ guilds/src/components/Profile_Borrowed.js
@@ -3,11 +3,18 @@ import './styles/profile.css';
 import axios from 'axios';
 import { format, parseISO } from 'date-fns';
 import Spinner from './layout/spinner_transparent.gif';
+import noimage from '../images/noimageavailable.png';
+
+function parsePath(orig) {
+  let res = orig.substr(9);
+  res = '.' + res;
+  return res;
+}
 
 class Profile_Borrowed extends Component {
   constructor(props) {
     super(props);
-    console.log('props profile borr rec',this.props)
+    console.log('props profile borr rec', this.props)
     this.state = {
       isLoading: true,
       listings: [],
@@ -15,9 +22,9 @@ class Profile_Borrowed extends Component {
     };
   }
   async componentDidMount() {
-    console.log('http://localhost:4000/market-place/borrowed/'+this.props.userid)
+    console.log('http://localhost:4000/market-place/borrowed/' + this.props.userid)
     const response = await axios.get(
-      'http://localhost:4000/market-place/borrowed/'+this.props.userid
+      'http://localhost:4000/market-place/borrowed/' + this.props.userid
     );
     console.log('profile borrowed', response);
     this.setState({ listings: response.data, isLoading: false });
@@ -34,7 +41,9 @@ class Profile_Borrowed extends Component {
             return (
               <div className='item' key={listing.item_id}>
                 <div className='itemImageWrapper'>
-                  <h1> img {listing.image} </h1>
+
+                  {listing.image != null ? <img src={parsePath(listing.image)} height="100" width="100"></img>
+                    : <img src={noimage} height="100" width="100"></img>}
                 </div>
                 <div className='itemInfoWrapper' key={listing}>
                   <h1 className='itemInfoField'> Name: {listing.item_name}</h1>
@@ -49,14 +58,14 @@ class Profile_Borrowed extends Component {
                     )}
                   </h1>
                   {this.props.canedit == true ? (
-                  <h1 className='itemInfoField'>
-                  {' '}
-                  Return By Date:{' '}
-                  {format(parseISO(listing.return_by), 'MMMM do,yyyy H:mma')}
-                </h1>
-              ):
-              ( console.log(''))
-              }
+                    <h1 className='itemInfoField'>
+                      {' '}
+                      Return By Date:{' '}
+                      {format(parseISO(listing.return_by), 'MMMM do,yyyy H:mma')}
+                    </h1>
+                  ) :
+                    (console.log(''))
+                  }
 
                   <hr />
                 </div>
@@ -64,10 +73,10 @@ class Profile_Borrowed extends Component {
             );
           })
         ) : (
-          <div className='spinner'>
-            <img src={Spinner} alt="loading..." />
-          </div>
-        )}
+            <div className='spinner'>
+              <img src={Spinner} alt="loading..." />
+            </div>
+          )}
       </React.Fragment>
     );
   }
