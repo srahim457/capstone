c1a31b11891e0fd4e361241aa92f13b51dbf6026
Louis-Ramirez <Louis.Ramirez60@myhunter.cuny.edu>
2020-05-19 13:57:02 -0400

    made some image changes

--- guilds/src/components/MarketPlaceSearch.js
+++ guilds/src/components/MarketPlaceSearch.js
@@ -2,14 +2,21 @@ import React, { Component } from 'react';
 import Pagination from './Pagination';
 import CreateListing from './CreateListing';
 import DisplayListing from './DisplayListing';
-import noimage from '../images/noimageavailable.png'
+import noimage from '../images/noimageavailable.png';
 import axios from 'axios';
 import './styles/MarketPlace.css';
 
-class MarketPlace extends Component {
+function parsePath(orig) {
+  let res = orig.substr(9);
+  res = '.' + res;
+  return res;
+}
+
+
+class MarketPlaceSearch extends Component {
   constructor() {
     super();
-    this.state ={
+    this.state = {
       isLoading: true,
       listings: [],
       error: null,
@@ -20,7 +27,7 @@ class MarketPlace extends Component {
       description: '',
       insurance: '',
       return_date: '',
-      images: '',
+      images: null,
       listing_type: '',
       pageOfItems: [],
     }
@@ -45,17 +52,17 @@ class MarketPlace extends Component {
 
   openListing(item) {
     console.log('open listing wth item', item)
-    this.setState({name: item.item_name});
-    this.setState({description: item.item_desc});
-    this.setState({listing_type: item.type})
-    this.setState({insurance: item.insurance_amount});
-    this.setState({images: item.image});
+    this.setState({ name: item.item_name });
+    this.setState({ description: item.item_desc });
+    this.setState({ listing_type: item.type })
+    this.setState({ insurance: item.insurance_amount });
+    this.setState({ images: item.image });
     this.setState({ open: true });
-    if(item.return_by === null){
-      this.setState({return_date: ''});
+    if (item.return_by === null) {
+      this.setState({ return_date: '' });
     }
-    else{
-      this.setState({return_date: item.return_by});
+    else {
+      this.setState({ return_date: item.return_by });
     }
   }
 
@@ -68,62 +75,72 @@ class MarketPlace extends Component {
   // }
 
   async componentDidMount() {
-    const response = await axios.get('http://localhost:4000/market-place/search/'+this.props.location.data)
+    const response = await axios.get('http://localhost:4000/market-place/search/' + this.props.location.data)
     console.log('listings', response)
-    this.setState({listings: response.data, isLoading: false})
+    this.setState({ listings: response.data, isLoading: false })
   }
 
   render() {
-    const {isLoading} = this.state;
-    const {data} = this.props.location;
+    const { isLoading } = this.state;
+    const { data } = this.props.location;
 
-    if(this.state.open === true){
+    if (this.state.open === true) {
       return <DisplayListing
-              name = {this.state.name}
-              description = {this.state.description}
-              return_date = {this.state.return_date}
-              insurance = {this.state.insurance}
-              listing_type = {this.state.listing_type}
-              />;
+        name={this.state.name}
+        description={this.state.description}
+        return_date={this.state.return_date}
+        insurance={this.state.insurance}
+        listing_type={this.state.listing_type}
+        images={this.state.images}
+      />;
     }
     return (
       <div className='containerParent'>
-      <div className='container'>
-        <h1 className='title'>Search Results: {data}</h1>
+        <div className='container'>
+          <h1 className='title'>Search Results: {data}</h1>
 
-        <div className='itemBoard'>
-          {console.log('test res',this.state.listings)}
-          <React.Fragment>
-            {!isLoading ? (
-              Object.values(this.state.listings).map(listing => {
-                return(
-                  <div className='itemContainer' key={listing.id}>
-                    {console.log('test listing',listing)}
-                    <div className='itemImage'
-                      onClick={this.openListing.bind(this, listing)}>
-                      {/*listing.image*/}
-                      <img src={noimage} height='150' width="200" ></img>
+          <div className='itemBoard'>
+            {console.log('test res', this.state.listings)}
+            <React.Fragment>
+              {!isLoading ? (
+                Object.values(this.state.listings).map(listing => {
+                  return (
+                    <div className='itemContainer' key={listing.id}>
+                      {console.log('test listing', listing)}
+                      <div className='itemImage'
+                        onClick={this.openListing.bind(this, listing)}>
+                        {/*listing.image*/}
+                        {listing.images != null ? (
+                          <img
+                            src={parsePath(listing.images)}
+                            height='200'
+                            width='200'
+                            alt={this.state.name}
+                          ></img>
+                        ) : (
+                            <img src={noimage} height='200' width="200" alt={this.state.name} ></img>
+                          )}
 
-                  </div>
-                  <h5>{listing.item_name}</h5>
-                  <div className='paginate'>
-                    <Pagination
-                    items={this.state.listing}
-                    onChangePage={this.onChangePage}
-                    />
-                  </div>
-                  </div>
-                );
-              })
-            ) : (
-            <h3>Loading</h3>
-              )}
+                      </div>
+                      <h5>{listing.item_name}</h5>
+                      <div className='paginate'>
+                        <Pagination
+                          items={this.state.listing}
+                          onChangePage={this.onChangePage}
+                        />
+                      </div>
+                    </div>
+                  );
+                })
+              ) : (
+                  <h3>Loading</h3>
+                )}
             </React.Fragment>
-        </div>
+          </div>
 
-      </div>
+        </div>
       </div>
     );
   }
 }
-export default MarketPlace;
+export default MarketPlaceSearch;
